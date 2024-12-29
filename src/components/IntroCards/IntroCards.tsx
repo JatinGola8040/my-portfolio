b6e79780acd7bcb8bd476ab4f9a4f26c3e7c2746
JatinGola8040 <jatingola8040@gmail.com>
2024-12-29 23:21:57 +0530

    Intro card

--- src/components/IntroCards/IntroCards.tsx
+++ src/components/IntroCards/IntroCards.tsx
@@ -1,6 +1,5 @@
 import React, { useContext, useRef } from 'react';
 import { store } from '../../App';
-import { motion } from "framer-motion";
 import './IntroCards.scss';
 
 export default function IntroCards () {
@@ -11,8 +10,8 @@ export default function IntroCards () {
     
     <div className="container-intro">
       <section className={`big-card ${state.darkmode ? "dark-card" : "light-card"}`}>
-        <h2 className={`${state.darkmode ? "dark-heading" : "light-heading"}`}>{state.language === "english" ? "React-Entwickler an der Schnittstelle von Design und Code." : "Python Developer with Passion in AI and Machine Learning"}</h2>
-        <p className={`${state.darkmode ? "dark-eyebrow" : "light-eyebrow"}`}>{state.language === "english" ? "Angefangen mit HTML, CSS und JavaScript habe ich mir eigenständig das Programmieren beigebracht. Auch jenseits von JavaScript gibt es vieles zu entdecken: Heute arbeite ich mit TypeScript in React, verwende Redux oder Context zum State-Management und nutze SASS als Pre-Processor für CSS. Wie man sieht arbeite ich desweiteren gern mit Motion Libraries - bspw. Framer Motion." : "As a tech enthusiast, I am always looking for opportunities to learn and grow. Whether it’s on academic courses, personal projects, or collaborations, I’m always interested in expanding my horizons and staying abreast of the latest trends and innovations in AI and technology."}</p>
+        <h1 className={`${state.darkmode ? "dark-heading" : "light-heading"}`}>{state.language === "english" ? "React-Entwickler an der Schnittstelle von Design und Code." : "Python Developer with Passion in AI and Machine Learning"}</h1>
+        <h2 className={`${state.darkmode ? "dark-eyebrow" : "light-eyebrow"}`}>{state.language === "english" ? "Angefangen mit HTML, CSS und JavaScript habe ich mir eigenständig das Programmieren beigebracht. Auch jenseits von JavaScript gibt es vieles zu entdecken: Heute arbeite ich mit TypeScript in React, verwende Redux oder Context zum State-Management und nutze SASS als Pre-Processor für CSS. Wie man sieht arbeite ich desweiteren gern mit Motion Libraries - bspw. Framer Motion." : "As a tech enthusiast, I am always looking for opportunities to learn and grow. Whether it’s on academic courses, personal projects, or collaborations, I’m always interested in expanding my horizons and staying abreast of the latest trends and innovations in AI and technology."}</h2>
       </section>
 
     </div>
